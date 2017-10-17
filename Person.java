821c59e6e30132bf5be547a2a159f4039ca9af02
Marco Kiefer <kiefer.marco@me.com>
2017-10-17 12:21:08 +0200

    initial commit, class Person.java added

---
+++ Person.java
@@ -0,0 +1,38 @@
+
+/**
+ * Beschreiben Sie hier die Klasse Person.
+ * 
+ * @author Marco Kiefer 
+ * @version 16.10.2017
+ */
+public class Person {
+    // Datenfelder
+    private String name;
+    private int alter;
+    
+    // Konstruktor keinen Rückgabetype, Name wie Klasse
+    // default oder parameterloser Konstruktor
+    public Person() {
+        this.name = "-kein Name festgelegt-";
+        this.alter = 0;
+    }
+    
+    public Person(String name) {
+        this.name = name;
+        this.alter = 0;
+    }
+    
+    // Überladen: gleicher Name, aber andere Parameter (Typ)
+    public Person(String name, int alter) {
+        this.name = name; //Verdeckung: Datenfeldname durch Parameter name verdeckt) (this.)
+        this.alter = alter;
+    }
+    
+    //Getter Methoden
+    public String getName () {
+     return name;   
+    }
+    public int getAlter () {
+     return alter;   
+    }
+} 
